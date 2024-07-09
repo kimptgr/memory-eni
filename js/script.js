4b1f73a0d4e37d9465687b61936bdc8ef6f77b3b
Kim Patingre <patingrekim@gmail.com>
2024-07-09 08:54:45 +0200

    update navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 window.onload = init ;
 var cards ;
+var btnStart ;
 function init(){
     cards = document.querySelectorAll(".memoryCard") ;
+    btnStart = document.querySelector(".btnStart")("click", playgame)
     showAllCards();
+    
 }
 
 function showAllCards(){
@@ -10,12 +13,15 @@ function showAllCards(){
     console.log(cards)
     cards.forEach((card, index) => {
         if (index < 6 ) {
-            console.log('url("/images/ressources1/memory-legume/' + index + '.svg")') ;
-            card.style.backgroundImage = `url("/images/ressources1/memory-legume/${index + 1}.svg")`;
+            card.style.backgroundImage = `url("./images/ressources1/memory-legume/${index + 1}.svg")`;
             }
             else {
             console.log("hello");
-            card.style.backgroundImage = `url("/images/ressources1/memory-legume/${index - 5}.svg")`;
+            card.style.backgroundImage = `url("./images/ressources1/memory-legume/${index - 5}.svg")`;
         }
     } )
+}
+
+function playgame(){
+    
 }
