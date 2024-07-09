2bcfdfe644b84e1899bbe0bc780244701463c8fd
Kim Patingre <patingrekim@gmail.com>
2024-07-09 12:16:15 +0200

    waiting for timout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.onload = init ;
 var cards ;
 var btnStart ;
+var nbShot ;
+var firstCardCliked ;
+
 
 function init(){
     cards = document.querySelectorAll(".memoryCard") ;
@@ -23,6 +26,7 @@ function showAllCards(){
 }
 
 function playgame(){
+    nbShot = 0 ;
     cards.forEach((card, index) => {
         card.style.backgroundImage = "url(./images/ressources1/question.svg" ;
         card.style.order = getRandom(11) ;
@@ -38,8 +42,8 @@ function playgame(){
 }
 
 function clickCard(e){
+    let timeoutID1, timeoutID2 ;
     let cardNumber = e.target.id ;
-    let imageNumber = cardNumber ;
     let cardClicked = document.getElementById(cardNumber) ;
 
     if (cardClicked.classList.contains("visible"))
@@ -47,32 +51,51 @@ function clickCard(e){
         alert("Tu as déjà retourné cette carte, choisis en une autre.")
     }
     else {
-        cardClicked.setAttribute('class', "visible") ;
-        if (cardNumber > 6) imageNumber = imageNumber -6 ;
-        cardClicked.style.backgroundImage = `url("./images/ressources1/memory-legume/${imageNumber}.svg")`;
+        if (nbShot%2 === 0) {
+            clearTimeout(timeoutID1);
+            clearTimeout(timeoutID2);
+            makeVisible(cardNumber)
+            firstCardCliked = cardNumber ;
+        }
+        else { 
+            makeVisible(cardNumber);
+            if(firstCardCliked == cardNumber + 6 || firstCardCliked == cardNumber - 6 ){
+                console.log("Hey égalité !")
+            }
+            else {
+                console.log("2ème carte à jouer fausse")
+                makeVisible(cardNumber);
+                timeoutID1 = setTimeout(makeInvisible, 3000, firstCardCliked);
+                timeoutID2 = setTimeout(makeInvisible, 3000, cardNumber);
+            }
+            }
+        nbShot ++ ;
     }
-        // Vérifie si paire
+    // Vérifie si paire
     //     cards.forEach()
     // let searchEven = cards.filter((card) => card.classList.contains("visible")) ; 
     // if (searchEven.length != 0) {console.log("Une paire !")}
     // else {console.log("Pas paire...")}
-
-
-
-
-
-
-
-    // if (cardNumber <= 6 ) {
-    //     cardClicked.style.backgroundImage = `url("./images/ressources1/memory-legume/${cardNumber}.svg")`;
-    //     }
-    //     else {
-    //     cardClicked.style.backgroundImage = `url("./images/ressources1/memory-legume/${cardNumber - 6}.svg")`;
-    // }
-    console.log("Une carte est cliquée ! + " + e.target.id)
+    
 }
 function getRandom(max){
     let randomNumber = Math.floor(Math.random()*max) ;
     return randomNumber ;
 
+}
+
+function makeVisible(cardNumber){
+    let imageNumber = cardNumber ;
+
+    cards[cardNumber-1].classList.add("visible") ;
+    if (cardNumber > 6) {imageNumber = imageNumber -6 ;}
+    cards[cardNumber-1].style.backgroundImage = `url("./images/ressources1/memory-legume/${imageNumber}.svg")`;
+}
+
+function makeInvisible(cardNumber){
+    let imageNumber = cardNumber ;
+
+    cards[cardNumber-1].classList.remove("visible") ;
+    if (cardNumber > 6) {imageNumber = imageNumber -6 ;}
+    cards[cardNumber-1].style.backgroundImage = `url(./images/ressources1/question.svg`;
 }
